8c3b8d5ea40cbd73879cc25a4cd8621f59308f0b
danipetro21 <idany346@gmail.com>
2023-06-19 14:16:04 +0200

    add calculator class

--- src/main/java/org/lessons/java/calculator/Calculator.java
+++ src/main/java/org/lessons/java/calculator/Calculator.java
@@ -1,2 +1,34 @@
-package org.lessons.java.calculator;public class Calculator {
+package org.lessons.java.calculator;
+
+public class Calculator {
+
+
+    private int num1;
+    private int num2;
+
+    public float add(float num1, float num2){
+
+        float risultato = num1 + num2;
+        return risultato;
+
+    }
+
+
+    public float subtract(float num1, float num2){
+        float risultato = num1 - num2;
+        return risultato;
+    }
+
+
+    public float divide(float num1, float num2){
+        float risultato = num1 / num2;
+        return risultato;
+    }
+
+
+    public float multiply(float num1, float num2){
+        float risultato = num1 * num2;
+        return risultato;
+    }
+
 }
